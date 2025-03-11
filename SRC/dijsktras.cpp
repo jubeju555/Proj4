59b5ee68f8d0e3cb14954c48021b705505438a5a
judah benjamin <142185545+jubeju555@users.noreply.github.com>
2025-03-11 14:03:42 +0000

    min distance should be done

--- SRC/dijsktras.cpp
+++ SRC/dijsktras.cpp
@@ -6,16 +6,52 @@
 #include <vector>
 #include <queue>
 #include <utility>
-
+#include <queue>
+#include <algorithm>
+#include <set>
+#include <climits>
 using namespace std;
-// need somethign to store weights
+// need something to store weights
 /*and need something to store graph - lets use a priority queue, since the smallest weight will just be at the top 
 we can use like a min heap and keep finding the smallest way to a encompass the "map" 
 */
+// pretty close to geeks4geeks might need to change to queue
+int mindist(){
+  set <int> visited;
+vector <int> dist;  
+int min = INT_MAX, min_index;
+    int verti = 0;
+    for (int i = 0; i < verti; i++)
+    {
+        if (visited.find(i) == visited.end() && dist[i] <= min)
+        {
+            min = dist[i];
+            min_index = i;
+        }
+    }
+    return min_index;
+}
+
+
 int dijsktrasalgo (){
+int weight = 0;
+int start = 0;
+int end = 0;
+
+
+
 
 }
 int main(int argc, char *argv[]) {
+   int weight, n;
+   int graphx, graphy;
+    while(cin >> n){
+    dijsktrasalgo();
+    // cout << "Enter the number of nodes: " << n << endl;
+    cin >> n;
+    cin >> graphx >> graphy;
+
+    }
     return 0;
 }
 
